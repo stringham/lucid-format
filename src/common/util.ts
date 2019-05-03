26de52b40df8622bc56e28e8a87bcfbcf0437cfa
Daniel James <daniel@danielscottjames.com>
2019-05-03 10:14:46 -0600

    check for tsconfig.build.json files as well (#2)

--- src/common/util.ts
+++ src/common/util.ts
@@ -6,8 +6,9 @@ export function getTsConfig(filePath: string) {
     let dir = path.dirname(filePath);
     let prevDir = filePath;
     while (dir != prevDir) {
-        const tsConfigPath = dir + '/tsconfig.json';
-        if (fs.existsSync(tsConfigPath)) {
+        const tsConfigPaths = [dir + '/tsconfig.build.json', dir + '/tsconfig.json'];
+        const tsConfigPath = tsConfigPaths.find(p => fs.existsSync(p));
+        if (tsConfigPath) {
             const config: any = ts.parseConfigFileTextToJson(tsConfigPath, fs.readFileSync(tsConfigPath).toString());
             config.path = tsConfigPath;
             return config;
