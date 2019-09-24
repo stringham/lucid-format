de74183f12443c85a0e1c67f40d85fea602d1e5c
Ryan Stringham <stringham@gmail.com>
2019-09-24 08:30:14 -0600

    Split variable declarations onto multiple lines.

--- src/lucidformat.ts
+++ src/lucidformat.ts
@@ -7,15 +7,17 @@ import {transform as replaceVars} from './novars';
 import {transform as missingSemicolons} from './semicolon';
 import {transform as trailingComma} from './trailingcommainitializer';
 import {transform as removeUnusedImports} from './unusedimports';
+import {transform as variableDeclaration} from './variabledeclaration';
 
 export function getLucidEdits(document: vscode.TextDocument, name: string): string {
     const contents = document.getText();
     const sourceFile = ts.createSourceFile(name, contents, ts.ScriptTarget.Latest, true);
     const transformFunctions = [
         missingSemicolons,
+        trailingComma,
+        variableDeclaration,
         replaceVars,
         combineImports,
-        trailingComma,
         removeUnusedImports,
     ];
     let replacements: Replacement[] = [];
