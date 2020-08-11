d607068b474d80080304ddde2e3b0e8acd3a404f
Sean McKenna <seanm@lucidchart.com>
2020-08-10 22:55:22 -0600

    Remove the initialize-enums transform
    This logic was replaced with an ESLint rule, specifically:
    `@typescript-eslint/prefer-enum-initializers`.

--- src/lucidformat.ts
+++ src/lucidformat.ts
@@ -3,14 +3,13 @@ import * as vscode from 'vscode';
 
 import {transform as combineImports} from './combineimports';
 import {combineReplacements, doReplacements, Replacement} from './common/replace';
-import {transform as initializeEnums} from './initializeenums';
 import {transform as removeUnusedImports} from './unusedimports';
 import {transform as enforceBraces} from './enforcebraces';
 
 export function getLucidEdits(document: vscode.TextDocument, name: string): string {
     const contents = document.getText();
     const sourceFile = ts.createSourceFile(name, contents, ts.ScriptTarget.Latest, true);
-    const transformFunctions = [combineImports, removeUnusedImports, initializeEnums, enforceBraces];
+    const transformFunctions = [combineImports, removeUnusedImports, enforceBraces];
     let replacements: Replacement[] = [];
     transformFunctions.forEach((f) => replacements.push(...f(sourceFile)));
     replacements = combineReplacements(replacements);
